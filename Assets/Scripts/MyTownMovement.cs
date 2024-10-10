529abe060edc628f7ed76a2f0ca62ea53b132833
Hongshinyoung <88022654+Hongshinyoung@users.noreply.github.com>
2024-10-10 11:26:26 +0900

    초기파일 업로드

---
+++ Assets/Scripts/MyTownMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MyTownMovement: MonoBehaviour
+{
+    private MyTownController controller;
+    private Rigidbody2D rigidbody;
+    private Vector2 movementDirection = Vector2.zero;
+    private float speed;
+
+    private void Awake()
+    {
+        controller = GetComponent<MyTownController>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        movementDirection *= speed;
+        rigidbody.velocity = movementDirection;
+    }
+
+}
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController: MyTownController
+{
+    Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    private void OnLook(InputValue value)
+    {
+        Vector2 lookInput = value.Get<Vector2>();
+        Vector2 worldPos = camera.ScreenToWorldPoint(lookInput);
+        lookInput = (worldPos - (Vector2)transform.position).normalized;
+        CallLookEvent(lookInput);
+    }
+}
